124249d6028fc91e75c3c3a2a64c5373f2ab561d
Hamza Mubeen <eb.blaze47@gmail.com>
2020-01-03 12:31:17 +0500

    a component integrated and called on interface and is responsible for rendering all the buttons the first 2 buttons handelers are defined on this page and the reset are passed as props from interface along with 2 mehthods from the interface file that are called in the the two methods in this component to change the data of the interface component

---
+++ src/components/Buttons.vue
@@ -0,0 +1,59 @@
+<template>
+    <div claas="row" style="margin-top:30px; border: 1px solid #888888; box-shadow:2px 2px #888888">
+
+      <div v-if="startGame" ><button v-for="item in buttons" @click="item.func" :key="item.id" class="ali btn" :class="item.type" > {{item.name}} </button>
+      </div>
+      <button v-else class="ali btn btn-primary" @click="start" > Start Game </button>
+
+    </div>
+</template>
+
+<script>
+
+
+export default { 
+  props:{
+    monsterDam:Function,
+    playerDam:Function, 
+    startGame:Function,
+    start:Function,
+    onGiveUp:Function,
+    onHeal:Function},
+  data(){
+     return{
+          buttons:[
+            {name:"Attack", type:"btn-danger",id:1, func : this.onAttack },
+            {name:"Speacial Attack", type:"btn-warning",id:2, func :this.onSpeacial },
+            {name:"Heal Up", type:"btn-success",id:3, func:this.onHeal},
+            {name:"Give up", type:"btn-dark",id:4 ,func:this.onGiveUp }
+            ]
+     };
+  },
+  methods:{
+    
+    onAttack(){
+      this.playerDam();
+      this.monsterDam(10);
+    },
+
+    onSpeacial(){
+      this.playerDam();
+      this.monsterDam(14);
+    },
+
+    
+  }
+
+}
+
+</script>
+
+<style scoped>
+
+.ali{
+
+  margin:20px;
+  font-size: 20px;
+}
+
+</style>
