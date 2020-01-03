9b0c1f546b90913683dee9b845b84ad0a53538fd
Hamza Mubeen <eb.blaze47@gmail.com>
2020-01-03 12:35:31 +0500

    this file renders the damage dealt in each round by both parties and and renders them in the new Dom two arrays are passe to this component that are rendered on the DOM using for directive

---
+++ src/components/Status.vue
@@ -0,0 +1,39 @@
+<template>
+    <div  class="col" style="margin-top:30px; border: 1px solid #888888; box-shadow:2px 2px #888888">
+      <div  v-for="(item,index) in playerDamage" :key="index+'b'" >
+      <div id="player-dam" > {{item}} </div>
+      <div id="monster-dam"> {{monsterDamage[index]}} </div>
+    </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+      playerDamage:Function,
+      monsterDamage:Function
+      },
+    data(){
+        return{
+        
+        }
+    },
+    
+
+}
+</script>
+
+
+
+<style>
+#player-dam{
+  background-color: rgb(88, 209, 169);
+  display: block;
+  margin:10px;
+}
+#monster-dam{
+  background-color: rgb(167, 68, 68);
+  display: block;
+  margin:10px;
+}
+</style>
